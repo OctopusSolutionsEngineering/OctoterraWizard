8a5668331a90f9f4c9bfb820b10e75e0d8e5ee37
Matthew Casperson <matthewcasperson@gmail.com>
2025-03-20 13:35:29 +1000

    Extracted sensitive variables

--- internal/sensitivevariables/database.go
+++ internal/sensitivevariables/database.go
@@ -1,6 +1,112 @@
 package sensitivevariables
 
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/mcasperson/OctoterraWizard/internal/naming"
+	"log"
+	"strings"
+	"time"
+)
+
 // ExtractVariables extracts sensitive variables from the database and returns them as terraform variable values
-func ExtractVariables() (string, error) {
-	return "", nil
+func ExtractVariables(server string, port int, database string, username string, password string, masterKey string) (string, error) {
+	ctx, stop := context.WithCancel(context.Background())
+	defer stop()
+
+	dsn := "sqlserver://" + username + ":" + password + "@" + server + ":" + fmt.Sprint(port) + "?database=" + database
+	db, err := sql.Open("sqlserver", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	db.SetConnMaxLifetime(0)
+	db.SetMaxIdleConns(3)
+	db.SetMaxOpenConns(3)
+
+	// Test the connection
+	if err := pingDatabase(ctx, db); err != nil {
+		return "", err
+	}
+
+	// Get the sensitive variables
+	sensitiveVars, err := getVariableSetSecrets(ctx, db, masterKey)
+
+	return sensitiveVars, err
+}
+
+func pingDatabase(ctx context.Context, db *sql.DB) error {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	return db.PingContext(ctx)
+}
+
+func getVariableSetSecrets(ctx context.Context, db *sql.DB, masterKey string) (string, error) {
+	var id string
+	var jsonValue string
+
+	timeout, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+	rows, err := db.QueryContext(timeout, "SELECT id, json FROM VariableSet")
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		if err := rows.Close(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	var builder strings.Builder
+
+	for rows.Next() {
+		if err = rows.Scan(&id, &jsonValue); err != nil {
+			return "", err
+		}
+
+		var result map[string]interface{}
+
+		if err := json.Unmarshal([]byte(jsonValue), &result); err != nil {
+			return "", err
+		}
+
+		if variables, ok := result["Variables"].([]interface{}); ok {
+			for _, variable := range variables {
+
+				if variableMap, ok := variable.(map[string]interface{}); ok {
+
+					if fmt.Sprint(variableMap["Type"]) != "Sensitive" {
+						continue
+					}
+
+					variableName := naming.VariableSecretName(fmt.Sprint(variableMap["Id"]))
+					variableValue, err := DecryptSensitiveVariable(masterKey, fmt.Sprint(variableMap["Value"]))
+
+					if err != nil {
+						return "", err
+					}
+
+					escapedValue, err := json.Marshal(variableValue)
+
+					if err != nil {
+						return "", err
+					}
+
+					builder.WriteString(variableName + " = " + string(escapedValue) + "\n")
+
+				}
+			}
+		}
+
+	}
+
+	return builder.String(), nil
 }
