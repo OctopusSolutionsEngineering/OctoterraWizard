d752b307faa537020a7bfc59f0d635aa8950f1d9
Matthew Casperson <matthewcasperson@gmail.com>
2025-05-06 10:03:34 +1000

    Port must be a number

--- internal/validators/mssql.go
+++ internal/validators/mssql.go
@@ -5,12 +5,18 @@ import (
 	"github.com/mcasperson/OctoterraWizard/internal/sensitivevariables"
 	"github.com/mcasperson/OctoterraWizard/internal/state"
 	"log"
+	"strconv"
 )
 
 // ValidateDatabase pings the database to confirm the connection details are correct
 func ValidateDatabase(state state.State) error {
 	ctx, stop := context.WithCancel(context.Background())
 	defer stop()
+
+	// Port must be a number
+	if _, err := strconv.Atoi(state.DatabasePort); err != nil {
+		return err
+	}
 
 	db, err := sensitivevariables.GetDatabaseConnection(state.DatabaseServer, state.DatabasePort, state.DatabaseName, state.DatabaseUser, state.DatabasePass, ctx)
 	if err != nil {
